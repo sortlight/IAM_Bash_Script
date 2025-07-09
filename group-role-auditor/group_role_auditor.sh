1718be76d48ce940cec52ed80ddaca2bee93b4e2
Sortsec <149396850+sortlight@users.noreply.github.com>
2025-07-08 17:55:41 -0700

    Update group_role_auditor.sh

--- group-role-auditor/group_role_auditor.sh
+++ group-role-auditor/group_role_auditor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#  IAM: Group & Role Auditor
-# Author: Gabriel
+# IAM: Group & Role Auditor
+# Author: Sortlight
 # Description: Lists system users and their group memberships.
 
 output_file="group_audit_report.txt"
@@ -9,6 +9,7 @@ echo " Auditing user group assignments..." > "$output_file"
 echo "--------------------------------------" >> "$output_file"
 
 # Loop through users with a shell (active users only)
+
 grep '/bin/bash' /etc/passwd | cut -d: -f1 | while read user; do
     groups=$(id -nG "$user")
     echo "👤 $user → $groups" >> "$output_file"
